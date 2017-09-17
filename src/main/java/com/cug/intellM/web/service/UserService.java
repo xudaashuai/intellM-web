bc6d63c691832d15ca0ad6a6bc4635fa88d24d0b
coldwater007 <31716812+coldwater007@users.noreply.github.com>
2017-09-17 11:56:42 +0800

    Update UserService.java
    基于dao层的数据库基本功能，实现了一些基本service

--- src/main/java/com/cug/intellM/web/service/UserService.java
+++ src/main/java/com/cug/intellM/web/service/UserService.java
@@ -1,10 +1,20 @@
 package com.cug.intellM.web.service;
 
-import java.util.List;
 import com.cug.intellM.web.po.User;
 
 
-public interface UserService {
+public interface UserService 
+{
 
-	public List<User> findUser();
+	
+	// 账号loginname 唯一
+	public boolean checkloginName(String loginName);//查询账号是否存在 是返回true
+	public boolean checkPassword(String loginName,String Password);//验证登陆密码 成功返回true，(已经登陆也返回false)  登陆成功将设置状态为1(已经登陆)
+	public int getUser_Type(String loginName);//验证成功后，调用此函数获取用户类型 0 普通用户 1 管理员
+	public User getUserByLoginName(String LoginName);//根据登陆账号查询用户信息
+	public boolean insertUser(String Name,int Sex,String LoginName,String password,String Dept_ID);//新增普通用户 dept_id 所属部门
+	public boolean insertAdministrator(String Name,int Sex,String LoginName,String password,String Dept_ID);//新增管理员
+	public boolean updateName(String loginname,String newname);//修改昵称
+	public boolean updatePassword(String loginname,String password);//修改密码
 }
+
